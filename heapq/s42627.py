71af920313e01d5d955567e5c8ef5e8c17050cfa
69429 <jwjun@lgcns.com>
2020-10-17 22:27:12 +0900

    s42627 수정

--- heapq/s42627.py
+++ heapq/s42627.py
@@ -13,10 +13,11 @@ def solution(jobs):
         heap = []
         for job in jobs:
             end = max(start, job[0]) + job[1]
-            heapq.heappush(heap, (end, job))
-        start, _job = heapq.heappop(heap)
+            heapq.heappush(heap, (max(start, job[0]), end, job))
+        _start, _end, _job = heapq.heappop(heap)
         jobs.remove(_job)
-        temp.append((start, _job))
+        temp.append((_end, _job))
+        start = _end
 
     _sum = 0
     for t in temp:
@@ -32,3 +33,12 @@ if __name__ == '__main__':
     result = solution([[0, 3], [1, 9], [2, 6]])
     print(result)
     assert result == 9
+
+    assert solution([[0, 3], [4, 3], [10, 3]] ) == 3
+    assert solution([[0, 10], [2, 3], [9, 3]]) == 9
+    assert solution([[1, 10], [3, 3], [10, 3]] ) == 9
+    assert solution([[0, 10]] ) == 10
+    assert solution([[0, 10], [4, 10], [5, 11], [15, 2]]) == 15
+    assert solution([[0, 1], [1, 2], [500, 6]]) == 3
+    assert solution([[0, 9], [0, 4], [0, 5], [0, 7], [0, 3]]) == 13
+    assert solution([[0, 5], [1, 2], [5, 5]]) == 6
