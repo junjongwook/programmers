5048342f5adcf309b775751504238ec003d1e51b
69429 <jwjun@lgcns.com>
2020-10-19 18:26:44 +0900

    uniPath 문제라 생각하고 간단하게 풀었는데, 테스트케이스 들에서 실패로 나오네

---
+++ DFS_BFS/s43162.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+def solution(n, computers):
+    answer = 0
+    temp = list(range(n))
+
+    for i in range(n):
+        for j in range(n):
+            if computers[i][j] == 1 and temp[i] > temp[j]:
+                temp[i] = temp[j]
+
+    answer = len(set(temp))
+
+    return answer
+
+
+if __name__ == '__main__':
+    result = solution(3, [[1, 1, 0], [1, 1, 0], [0, 0, 1]])
+    print(f'result = {result}')
+    assert result == 2
+
+    result = solution(3, [[1, 1, 0], [1, 1, 1], [0, 1, 1]])
+    print(f'result = {result}')
+    assert result == 1
