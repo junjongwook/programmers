f1c874aeb8d86b1a21a82737ae6cf2fe3bd7acc0
69429 <jwjun@lgcns.com>
2020-10-19 17:05:39 +0900

    쉽게 풀었는데, 1,2 테스트케이스에서 시간초과 나오네. 그러면 계산을 미리 해두는 식으로.. 변경이 필요하겠네

---
+++ DFS_BFS/s43165.py
@@ -0,0 +1,30 @@
+# -*- coding : utf-8 -*-
+'''
+타겟 넘버 : https://programmers.co.kr/learn/courses/30/lessons/43165
+'''
+
+
+def solution(numbers, target):
+    answer = 0
+    queue = [[]]
+    for n in numbers:
+        temp_queue = []
+        while queue:
+            temp = queue.pop(0)
+            temp_queue.append(temp + [n])
+            temp_queue.append(temp + [-n])
+        queue = temp_queue
+
+    while queue:
+        _sum = sum(queue.pop(0))
+        if _sum == target:
+            answer += 1
+
+    return answer
+
+
+if __name__ == '__main__':
+    result = solution([1, 1, 1, 1, 1], 3)
+    print(f'result = {result}')
+    assert result == 5
+
