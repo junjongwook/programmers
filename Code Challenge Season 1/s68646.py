78c17af8379dc30e0f091a68d4a6e924d636c3fb
69429 <jwjun@lgcns.com>
2020-10-21 12:55:20 +0900

    heapify 까지 동원했는데도 시간초과네.. 아직도 아까 방법이 더 많은 점수가 나오고 이건 20점 밖에 안 나오네.

--- Code Challenge Season 1/s68646.py
+++ Code Challenge Season 1/s68646.py
@@ -3,20 +3,21 @@
 풍선 터트리기 : https://programmers.co.kr/learn/courses/30/lessons/68646?language=python3
 '''
 
+import heapq
+
 def solution(a):
     if len(a) < 3:
         return len(a)
     answer = 2
     left_min = a[0]
-    right_min = None # 필요할 때만 min 을 수행한다.
     for i in range(1, len(a) - 1):
         if left_min > a[i]: # 왼쪽이 모두 다 크면 그냥 오른쪽은 비교할 필요도 없다.
             answer += 1
             left_min = a[i]
-            continue
         else:   # 이제 오른쪽을 찾아 봐야 하는구나
-            right_min = min(a[i+1:])
-            if right_min > a[i]: # 이러면 성공!
+            temp = a[i+1:]
+            heapq.heapify(temp)
+            if temp[0] > a[i]: # 이러면 성공!
                 answer += 1
 
     return answer
