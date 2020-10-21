0430b56ada84dd4aab506de97391fae99b698c40
69429 <jwjun@lgcns.com>
2020-10-21 14:11:14 +0900

    하나 더 통과했을 뿐 여전히 시간초과가 많다.

--- Code Challenge Season 1/s68646.py
+++ Code Challenge Season 1/s68646.py
@@ -3,21 +3,32 @@
 풍선 터트리기 : https://programmers.co.kr/learn/courses/30/lessons/68646?language=python3
 '''
 
-import heapq
+import heapq, bisect
 
 def solution(a):
     if len(a) < 3:
         return len(a)
     answer = 2
     left_min = a[0]
+    right_heap = a[2:]
+    heapq.heapify(right_heap)
+    removed_list = []
     for i in range(1, len(a) - 1):
+        bisect.insort(removed_list, a[i])
         if left_min > a[i]: # 왼쪽이 모두 다 크면 그냥 오른쪽은 비교할 필요도 없다.
             answer += 1
             left_min = a[i]
         else:   # 이제 오른쪽을 찾아 봐야 하는구나
-            temp = a[i+1:]
-            heapq.heapify(temp)
-            if temp[0] > a[i]: # 이러면 성공!
+            right_min = right_heap[0]
+            while True:
+                index = bisect.bisect_left(removed_list, right_min)
+                if index == len(removed_list):
+                    break
+                elif removed_list[index] > right_min:
+                    break
+                heapq.heappop(right_heap)
+                right_min = right_heap[0]
+            if right_min > a[i]: # 이러면 성공!
                 answer += 1
 
     return answer
