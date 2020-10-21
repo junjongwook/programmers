9d0fe3058c2a11034e4ccffb6924a7e41f86e85e
69429 <jwjun@lgcns.com>
2020-10-21 15:13:23 +0900

    그나마 아까와 같은 점수 60 점이 나오는 코드네. 이걸 tuning 해야 하나?

--- Code Challenge Season 1/s68646.py
+++ Code Challenge Season 1/s68646.py
@@ -5,31 +5,50 @@
 
 import heapq, bisect
 
+
 def solution(a):
     if len(a) < 3:
         return len(a)
-    answer = 2
+    answer = 2  # 맨 앞과 맨 뒤는 무조건 가능하다
+    # 오른쪽에서 시작해서 작은 값들은 제거해서 개수를 줄여 본다.
+    length = len(a)
     left_min = a[0]
-    right_heap = a[2:]
-    heapq.heapify(right_heap)
-    removed_list = []
-    for i in range(1, len(a) - 1):
-        bisect.insort(removed_list, a[i])
-        if left_min > a[i]: # 왼쪽이 모두 다 크면 그냥 오른쪽은 비교할 필요도 없다.
+    right_under = a[length-1]
+    right_under_index = length - 1
+
+    for i in range(1, length - 1):
+        if left_min > a[i]: # 왼쪽에 작은 값이 없으면
             answer += 1
             left_min = a[i]
-        else:   # 이제 오른쪽을 찾아 봐야 하는구나
-            right_min = right_heap[0]
-            while True:
-                index = bisect.bisect_left(removed_list, right_min)
-                if index == len(removed_list):
+            continue
+        elif i < right_under_index and a[i] > right_under:  # 오른쪽에 작은 값이 있으면
+            continue
+        elif i < right_under_index: # 좀더 작은 값을 찾아 보려면
+            _check = False
+            for j in range(right_under_index - 1, i, -1):
+                if a[j] < a[i]: # 작은 값이 존재하면
+                    right_under_index = j
+                    right_under = a[j]
+                    _check = True
                     break
-                elif removed_list[index] > right_min:
-                    break
-                heapq.heappop(right_heap)
-                right_min = right_heap[0]
-            if right_min > a[i]: # 이러면 성공!
+            if _check: # 작은 값을 찾았으니
+                continue
+            else: # 오른쪽에 작은 값이 없으면
                 answer += 1
+                continue
+        else:   # 오른쪽의 작은값을 다시 찾아야 하는 경우
+            _check = False
+            for j in range(length - 1, i, -1):
+                if a[j] < a[i]:
+                    right_under_index = j
+                    right_under = a[j]
+                    _check = True
+                    break
+            if _check: # 작은 값을 찾았으니
+                continue
+            else:       # 오른쪽에 작은 값이 없으면
+                answer += 1
+                continue
 
     return answer
 
