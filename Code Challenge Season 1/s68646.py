eb8a9aeaed92bbfdd7f74ae76ecdba311a5c0562
69429 <jwjun@lgcns.com>
2020-10-21 12:47:10 +0900

    min 을 수행하는 횟수를 줄여 볼려고 했는데, 이것도 시간초과가 많이 나오네

--- Code Challenge Season 1/s68646.py
+++ Code Challenge Season 1/s68646.py
@@ -8,19 +8,16 @@ def solution(a):
         return len(a)
     answer = 2
     left_min = a[0]
-    right_sort = sorted(a[2:])
-    from collections import deque
-    right_sort = deque(right_sort)
-    right_sort.append(float('inf'))
-    right_min = right_sort[0]
+    right_min = None # 필요할 때만 min 을 수행한다.
     for i in range(1, len(a) - 1):
-        if max(left_min, a[i], right_min) == a[i]:
-            pass
-        else:
-            answer = answer + 1
-        left_min = min(left_min, a[i])
-        right_sort.remove(a[i+1])
-        right_min = right_sort[0]
+        if left_min > a[i]: # 왼쪽이 모두 다 크면 그냥 오른쪽은 비교할 필요도 없다.
+            answer += 1
+            left_min = a[i]
+            continue
+        else:   # 이제 오른쪽을 찾아 봐야 하는구나
+            right_min = min(a[i+1:])
+            if right_min > a[i]: # 이러면 성공!
+                answer += 1
 
     return answer
 
