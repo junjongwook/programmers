294d57015ae54df47ccfdc2ef6979db35448d799
69429 <jwjun@lgcns.com>
2020-10-23 14:52:51 +0900

    헤매다가 Floyd warshall 방법으로 최단 거리 구하는 건 쉽게 했는데, 8번째 케이스부터 시간초과가 나오네.

--- Code Challenge Season 1/s68937.py
+++ Code Challenge Season 1/s68937.py
@@ -7,34 +7,32 @@
 def solution(n, edges):
     answer = 0
     link = [[float('inf')] * (n + 1) for _ in range(n + 1)]
-    # 인자 정리
+    for i in range(n + 1):
+        link[i][i] = 0
     for y, x in edges:
         link[y][x] = 1
         link[x][y] = 1
-    # 같은 node 는 거리가 0
-    for _n in range(n + 1):
-        link[_n][_n] = 0
 
-    # node 간의 최소 거리를 계산
-    for y in range(1, n + 1):
-        for x in range(y + 1, n + 1):
-            if link[y][x] < float('inf'):
-                for _x in range(1, n + 1):
-                    if link[x][_x] < float('inf'):
-                        link[y][_x] = min(link[y][x] + link[x][_x], link[y][_x])
-                        link[_x][y] = link[y][_x]
+    # Floyd-warshall
+    for m in range(1, n + 1):
+        for s in range(1, n + 1):
+            if s == m: continue
+            for e in range(1, n + 1):
+                if e == m: continue
+                if s == e: continue
+                link[s][e] = min(link[s][m] + link[m][e], link[s][e])
+                link[e][s] = link[s][e]
 
     # 이제 번호 3개를 추출해서 거리의 중간을 가지고 배열을 만든다.
-    temp = []
     from itertools import combinations
+    answer = 0
     for t in combinations(range(1, n + 1), 3):
         d1 = link[t[0]][t[1]]
         d2 = link[t[0]][t[2]]
         d3 = link[t[1]][t[2]]
-        m = sorted([d1, d2, d3])[1]
-        temp.append(m)
+        if sorted([d1, d2, d3])[1] > answer:
+            answer = sorted([d1, d2, d3])[1]
 
-    answer = max(temp)
     return answer
 
 
