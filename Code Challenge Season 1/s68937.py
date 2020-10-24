34686b4c144ad1a8518fd269270cf5a5b471e9a9
69429 <jwjun@lgcns.com>
2020-10-24 16:50:20 +0900

    완전히 다르게, 트리 지름이란 걸 적용해서 했는데, 실패 케이스 몇개가 나오고 시간초과가 하나 나온다. BFS 가 아니라 DFS 로 해봐야 겠다.

--- Code Challenge Season 1/s68937.py
+++ Code Challenge Season 1/s68937.py
@@ -6,31 +6,51 @@
 
 def solution(n, edges):
     answer = 0
-    from bisect import insort
-    temp = []
-    link = [[float('inf')] * (n + 1) for _ in range(n + 1)]
-    for i in range(n + 1):
-        link[i][i] = 0
-    for y, x in edges:
-        link[y][x] = 1
-        link[x][y] = 1
-        insort(temp, 1)
+    edgeDict = dict()
+    for v1, v2 in edges:
+        edgeDict.setdefault(v1, [])
+        edgeDict.setdefault(v2, [])
+        edgeDict[v1].append(v2)
+        edgeDict[v2].append(v1)
 
-    # Floyd-warshall
-    for m in range(1, n + 1):
-        for s in range(1, n + 1):
-            if s == m: continue
-            for e in range(1, n + 1):
-                if e == m: continue
-                if s == e: continue
-                if s > e: continue
-                if link[s][e] == float('inf'):
-                    _temp = link[s][m] + link[m][e]
-                    if _temp != float('inf'):
-                        link[s][e] = _temp
-                        link[e][s] = _temp
-                        insort(temp, _temp)
-    answer = temp[-2]
+    # 첫번째 노드에서 제일 먼 곳의 노드를 하나 찾는다.
+    start = edges[0][0]
+    from collections import deque
+    queue = deque([[start]])
+    end = start
+    while queue:
+        vertices = queue.popleft()
+        end = vertices[-1]
+        for next in edgeDict[end]:
+            if next not in vertices:
+                _temp = vertices.copy()
+                _temp.append(next)
+                queue.append(_temp)
+
+    # 찾은 노드에서 다시 제일 먼 노드를 찾는다. 이 때 거리가 트리의 지름이 된다.
+    start = end
+    queue = deque([[start]])
+    maxDistance = 0
+    end = start
+    while queue:
+        vertices = queue.popleft()
+        _distance = len(vertices) - 1
+        if maxDistance < _distance:
+            maxDistance = _distance
+            check = False   # 새로 등장한 거리라면 값이 중복되지 않는다.
+        else:
+            check = True    # 이전 값과 동일하므로 값이 중복된다. 나중에 보면 트리 지름이 2개 이상이 된다.
+        end = vertices[-1]
+        for next in edgeDict[end]:
+            if next not in vertices:
+                _temp = vertices.copy()
+                _temp.append(next)
+                queue.append(_temp)
+
+    if check:   # 트리 지름이 2개 이상 있을 경우
+        answer = maxDistance
+    else:
+        answer = maxDistance - 1
 
     return answer
 
