ad28b24bcbd158d43dad22eca8f4addfe62054b3
69429 <jwjun@lgcns.com>
2020-10-26 18:14:12 +0900

    실패가 몇 개 나온다. 논리가 잘못되었나?

---
+++ Skill Check/Level3/s199903_2.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+
+def solution(m, n, puddles):
+    BASE = 1_000_000_007
+    ground = [[0] * (m + 1) for _ in range(n + 1)]
+    for i in range(2, m + 1):
+        if [i, 1] in puddles: continue
+        ground[1][i] = 1
+    for j in range(2, n + 1):
+        if [1, j] in puddles: continue
+        ground[j][1] = 1
+
+    for _n in range(2, n + 1):
+        for _m in range(2, m + 1):
+            if [_m, _n] in puddles:
+                continue
+            ground[_n][_m] = ( ground[_n][_m - 1] + ground[_n - 1][_m] ) % BASE
+
+    answer = ground[n][m]
+    return answer
+
+
+if __name__ == '__main__':
+    result = solution(4, 3, [[2, 2]])
+    print(f'result={result}')
+    assert result == 4
+
