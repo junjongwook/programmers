0b1b0aaaf77b6f7fb9eec653dadf2aa603d40447
69429 <jwjun@lgcns.com>
2020-11-19 00:40:33 +0900

    쓸데없이 class 로 처리해서 어렵게 갔네. 다 맞는거 같은데 30번 케이스가 실패다.

---
+++ Skill Check/Level2/s17683.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+"""
+[3차]방금그곡 : https://programmers.co.kr/learn/courses/30/lessons/17683?language=python3
+"""
+
+def change(m):
+    m = m.replace('C#', 'c')
+    m = m.replace('D#', 'd')
+    m = m.replace('F#', 'f')
+    m = m.replace('G#', 'g')
+    m = m.replace('A#', 'a')
+    return m
+
+def solution(m, musicinfos):
+    answer = '(None)'
+    m = change(m)
+
+    class MusicInfo:
+        def __init__(self, data):
+            self.start, self.end, self.title, self.music = data.split(',')
+            self.music = change(self.music)
+
+        def match(self, m):
+            temp = self.music
+            while len(temp) < self.elapsed():
+                temp += self.music
+
+            return m in temp
+
+        def elapsed(self):
+            '''
+            연주되는 음의 개수
+            :return: 음의 개수
+            '''
+            _start = int(self.start[0:2]) * 60 + int(self.start[3:5])
+            _end = int(self.end[0:2]) * 60 + int(self.end[3:5])
+            return _end - _start
+
+    _match = None
+    for musicinfo in musicinfos:
+        _m = MusicInfo(musicinfo)
+        if _m.match(m):
+            if _match is None:
+                _match = _m
+                answer = _m.title
+            elif _match.elapsed() < _m.elapsed():
+                _match = _m
+                answer = _m.title
+
+    return answer
+
+
+if __name__ == '__main__':
+    # result = solution("ABCDEFG", ["12:00,12:14,HELLO,CDEFGAB", "13:00,13:05,WORLD,ABCDEF"])
+    # print(f'result = {result}')
+    # assert result == "HELLO"
+    #
+    # result = solution("ABC", ["12:00,12:14,HELLO,C#DEFGAB", "13:00,13:05,WORLD,ABCDEF"])
+    # print(f'result = {result}')
+    # assert result == "WORLD"
+
+    result = solution("CC#BCC#BCC#BCC#B", ["03:00,03:30,FOO,CC#B", "04:00,04:08,BAR,CC#BCC#BCC#B"])
+    print(f'result = {result}')
+    assert result == "FOO"
