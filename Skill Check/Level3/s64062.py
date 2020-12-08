c093b011ef8587a03a2767c50e0286a75559e11c
69429 <jwjun@lgcns.com>
2020-12-08 13:29:12 +0900

    이분법으로 푸는게 맞네. 로직을 조금 개선했더니 다 맞게 나오네

--- Skill Check/Level3/s64062.py
+++ Skill Check/Level3/s64062.py
@@ -8,13 +8,13 @@ def solution(stones, k):
     answer = 0
     length = len(stones)
     _min, _max = min(stones), max(stones)
-    while _min < _max:
+    while _min <= _max:
         _mid = (_min + _max) // 2
         zero = [0] * (length + 1)
         for i in range(1, length + 1):
-            if stones[i-1] - _mid <= 0:
+            if stones[i-1] - _mid < 0:
                 zero[i] = zero[i-1] + 1
-                if zero[i] > k:
+                if zero[i] >= k:
                     _max = _mid - 1
                     break
         else:
