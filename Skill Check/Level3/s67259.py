af5b65b496d87735aa964fee8284a3442639914f
69429 <jwjun@lgcns.com>
2020-12-09 22:14:03 +0900

    BFS 로 풀긴 했는데 65.2 밖에 안 나오네. 로직은 문제가 없긴 하지만 다시 DFS 로 해야 하나?

--- Skill Check/Level3/s67259.py
+++ Skill Check/Level3/s67259.py
@@ -2,68 +2,50 @@
 """
 경주로건설 : https://programmers.co.kr/learn/courses/30/lessons/67259?language=python3
 """
-import sys
-answer = float('inf')
-index = 0
+from collections import deque
+
 
 def solution(board):
-    dxy = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+    answer = float('inf')
 
+    dh = [0, 1, 0, -1]
+    dw = [1, 0, -1, 0]
     N = len(board)
 
-    visited = [[0] * N for _ in range(N)]
+    queue = deque([])
+    queue.append([[(0, 0)], -1, 0])     # 현재 위치, 이전 방향, 현재까지 비용
 
-    def canGo(h, w):
-        if h < 0 or w < 0: return False
-        if h >= N or w >= N: return False
-        if board[h][w] == 1: return False
-        if visited[h][w] == 1: return False
-
-        return True
-
-    def DFS(curr, d, total):
-        global index
-        print(f'curr = {curr}, d = {d}, total = {total}')
-        index = index + 1
-        if index > 1000: sys.exit(0)
-
-        global answer
-        _h = curr[0] + dxy[d][0]
-        _w = curr[1] + dxy[d][1]
-        if not canGo(_h, _w):
-            return
-
-        if _h == N - 1 and _w == N - 1:
+    while queue:
+        paths, direction, total = queue.popleft()
+        if paths[-1] == (N - 1, N - 1):
             answer = min(answer, total)
-            return
-
-        visited[_h][_w] = 1
-        for _d in range(4):
-            if _d == d: DFS((_h, _w), _d, total + 100)
-            else:
-                DFS((_h, _w), _d, total + 100 + 500)
-        visited[_h][_w] = 0
-
-        return
-
-    visited[0][0] = 1
-    DFS([0, 0], 0, 100)
-    DFS([0, 0], 1, 100)
-    DFS([0, 0], 2, 100)
-    DFS([0, 0], 3, 100)
-    visited[0][0] = 0
+            # print(f'paths = {paths}, direction = {direction}, total = {total}')
+            continue
+        h, w = paths[-1]
+        for d in range(4):
+            _h = h + dh[d]
+            _w = w + dw[d]
+            if (_h, _w) in paths:
+                continue
+            if 0 <= _h < N and 0 <= _w < N and board[_h][_w] == 0:
+                _total = total + 100
+                if direction != -1 and d != direction:
+                    _total += 500
+                if _total > answer:
+                    continue
+                queue.append([paths + [(_h, _w)], d, _total])
 
     return answer
 
 
 if __name__ == '__main__':
-    # result = solution([[0, 0, 0], [0, 0, 0], [0, 0, 0]])
-    # print(f'result = {result}')
-    # assert result == 900
+    result = solution([[0, 0, 0], [0, 0, 0], [0, 0, 0]])
+    print(f'result = {result}')
+    assert result == 900
 
-    # result = solution([[0, 0, 1, 0], [0, 0, 0, 0], [0, 1, 0, 1], [1, 0, 0, 0]])
-    # print(f'result = {result}')
-    # assert result == 2100
+    result = solution([[0, 0, 1, 0], [0, 0, 0, 0], [0, 1, 0, 1], [1, 0, 0, 0]])
+    print(f'result = {result}')
+    assert result == 2100
 
     result = solution([[0, 0, 0, 0, 0, 0, 0, 1], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 1, 0, 0], [0, 0, 0, 0, 1, 0, 0, 0], [0, 0, 0, 1, 0, 0, 0, 1], [0, 0, 1, 0, 0, 0, 1, 0], [0, 1, 0, 0, 0, 1, 0, 0], [1, 0, 0, 0, 0, 0, 0, 0]])
     print(f'result = {result}')
