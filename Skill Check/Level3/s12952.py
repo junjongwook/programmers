6eb9bbc71eca5ea04090d4a3abf34b6e6e5fbf8e
69429 <jwjun@lgcns.com>
2020-12-16 01:36:25 +0900

    DFS 로도 풀어진다.

--- Skill Check/Level3/s12952.py
+++ Skill Check/Level3/s12952.py
@@ -3,57 +3,46 @@
 N-Queen : https://programmers.co.kr/learn/courses/30/lessons/12952?language=python3
 """
 
-
+answer = 0
 def solution(n):
+    global answer
     answer = 0
 
-    def next2(x, y):
-        if x == y == -1:
-            return (0, 0)
-        _x = x + 1
-        if _x < n:
-            return (_x, y)
-        _y = y + 1
-        if _y < n:
-            return (0, _y)
-
-        return (-1, -1)
-
-    from collections import deque
-    queue = deque([])
-    # 시작 포인트를 모두 queue 에 넣어 둔다.
-    for y in range(n):
-        for x in range(n):
-            queue.append([(x, y)])
-
-    while queue:
-        qList = queue.popleft()
+    def DFS(qList):
+        global answer
         if len(qList) == n:
+            # print(f'qList = {qList}')
             answer = answer + 1
-            continue
 
-        nX, nY = qList[-1]
-        while True:
-            nX, nY = next2(nX, nY)
-            if nX == nY == -1:
-                break
-            for qX, qY in qList:
-                if qX == nX or qY == nY:
+        if len(qList) == 0:
+            y = 0
+        else:
+            y = qList[-1][1] + 1
+
+        for x in range(n):
+            for _x, _y in qList:
+                if _x == x:
                     break
-                if qX + qY == nX + nY:
+                if _x + _y == x + y:
                     break
-                if qX - qY == nX - nY:
+                if _x - _y == x - y:
                     break
             else:
-                newQList = qList.copy()
-                newQList.append((nX, nY))
-                queue.append(newQList)
+                qList.append((x, y))
+                DFS(qList)
+                qList.remove((x, y))
+
+    DFS([])
 
     return answer
-
 
 if __name__ == '__main__':
     result = solution(4)
     print(f'result = {result}')
     assert result == 2
+
+    result = solution(5)
+    print(f'result = {result}')
+    assert result == 10
+
 
