22c6f3b87dd06ea528970dea3e62100b16da021f
69429 <jwjun@lgcns.com>
2020-12-15 15:18:20 +0900

    BFS 로 해서 풀리는군

--- Skill Check/Level3/s67259.py
+++ Skill Check/Level3/s67259.py
@@ -2,43 +2,39 @@
 """
 경주로건설 : https://programmers.co.kr/learn/courses/30/lessons/67259?language=python3
 """
-answer = float('inf')
 
 
 def solution(board):
     global answer
-    answer = float('inf')
-    dh = [0, 1, 0, -1]
-    dw = [1, 0, -1, 0]
     N = len(board)
-    visited = [[0] * N for _ in range(N)]
-    visited[0][0] = 1
+    map = [[float('inf') for a in range(N)] for _ in range(N)]
+    from collections import deque
+    queue = deque()
+    # 방향 전환 ==> 0 : 오른쪽, 1 : 아래, 2 : 왼쪽, 3 : 위
+    dx = [1, 0, -1, 0]
+    dy = [0, 1, 0, -1]
 
-    def DFS(h, w, d, total):
-        global answer
-        # print(f'[({h}, {w}), {d}, {total}] -> ', end=' ')
-        if h == w == N - 1:
-            answer = min(answer, total)
-            # print(f'answer = {answer}')
-            return
+    queue.append((0, 0, 0, 100))
+    queue.append((0, 0, 1, 100))
+    map[0][0] = 0
 
-        for _d in range(4):
-            _h = h + dh[_d]
-            _w = w + dw[_d]
-            if 0 <= _h < N and 0 <= _w < N and board[_h][_w] == 0 and visited[_h][_w] == 0:
-                _total = total + 100
-                if _d != d:
-                    _total += 500
-                if _total > answer:
-                    continue
-                visited[_h][_w] = 1
-                DFS(_h, _w, _d, _total)
-                visited[_h][_w] = 0
+    while queue:
+        _x, _y, _d, _c = queue.popleft()
+        # print('_x, _y, _d, _c =', _x, _y, _d, _c, end=' ')
+        nx = _x + dx[_d]
+        ny = _y + dy[_d]
+        if 0 <= nx < N and 0 <= ny < N and board[ny][nx] == 0 and _c <= map[ny][nx]:
+            map[ny][nx] = _c
+            # print('ok')
+            for i in range(4):  # 4방향으로 가본다.
+                if _d == i:
+                    queue.append((nx, ny, i, _c + 100))
+                else:
+                    queue.append((nx, ny, i, _c + 600))
+        # else:
+        #     print()
 
-    if board[0][1] == 0:
-        DFS(0, 1, 0, 100)
-    if board[1][0] == 0:
-        DFS(1, 0, 1, 100)
+    answer = map[N-1][N-1]
 
     return answer
 
