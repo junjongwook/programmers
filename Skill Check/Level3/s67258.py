20c8ea41fb8231e0130eeca63d7eabc1d0e037ee
69429 <jwjun@lgcns.com>
2020-12-17 14:12:15 +0900

    뒤에서 하는 것부터 했더니, 구멍나는게 있어서 앞을 먼저도 해서 해결했는데 그것도 아니다. 아예 다른 방법을 강구하자.

--- Skill Check/Level3/s67258.py
+++ Skill Check/Level3/s67258.py
@@ -10,6 +10,9 @@ def solution(gems):
     for g in gems:
         d.setdefault(g, 0)
         d[g] += 1
+    d2 = d.copy()
+
+    answer = [1, width - 1]
 
     # 뒤에서 잘라본다.
     end = width
@@ -19,7 +22,6 @@ def solution(gems):
             break
         else:
             d[gems[i]] -= 1
-
     # 이제는 앞에서 잘라본다.
     start = 1
     for j in range(0, width):
@@ -29,7 +31,29 @@ def solution(gems):
         else:
             d[gems[j]] -= 1
 
-    return [start, end]
+    # 앞에서부터 자르는 걸 먼저 해 본다.
+    start2 = 1
+    for i in range(0, width):
+        if d2[gems[i]] == 1:
+            start2 = i + 1
+            break
+        else:
+            d2[gems[i]] -=1
+    # 뒤에서 자르는 걸 해 본다.
+    end2 = width
+    for j in range(width - 1, -1, -1):
+        if d2[gems[j]] == 1:
+            end2 = j + 1
+            break
+        else:
+            d2[gems[j]] -= 1
+
+    if end2 - start2 < end - start:
+        answer = [start2, end2]
+    else:
+        answer = [start, end]
+
+    return answer
 
 
 if __name__ == '__main__':
@@ -56,3 +80,7 @@ if __name__ == '__main__':
     result = solution(["A", "B", "A"])
     print(f'result = {result}')
     assert result == [1, 2]
+
+    result = solution( ["DIA", "EM", "EM", "RUB", "DIA"])
+    print(f'result = {result}')
+    assert result == [3, 5]
