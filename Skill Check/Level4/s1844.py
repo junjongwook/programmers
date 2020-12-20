8c325cb2951e7e41882bdc462ce29d94b7180bb7
69429 <jwjun@lgcns.com>
2020-12-21 02:32:21 +0900

    해당 위치에 옮겨온 값을 넣어 두고 비교를 해서 더 큰 값이나 같은 값은 더 이상 진행하지 않도록 했더니 효율성을 통과하네

--- Skill Check/Level4/s1844.py
+++ Skill Check/Level4/s1844.py
@@ -7,27 +7,32 @@
 def solution(maps):
     n = len(maps)
     m = len(maps[0])
+    visited = [[float('inf')] * m for _ in range(n)]
     dx = [1, 0, -1, 0]
     dy = [0, 1, 0, -1]
     answer = -1
     from collections import deque
     queue = deque()
-    queue.append([(0, 0)])
+    queue.append([[(0, 0)], 1])
+    visited[0][0] = 1
     while queue:
-        foot = queue.popleft()
-        # print(f'foot = {foot}')
-        if foot[-1] == (n-1, m-1):
-            answer = len(foot)
-            break
+        state = queue.popleft()
+        # print(f'state = {state}')
+        foot, distance = state
         y, x = foot[-1]
+        if (y, x) == (n-1, m-1):
+            answer = distance
+            break
         for i in range(4):
             _x = x + dx[i]
             _y = y + dy[i]
+            _distance = distance + 1
             if (_y, _x) in foot: continue
-            if 0 <= _y < n and 0 <= _x < m and maps[_y][_x] == 1:
+            if 0 <= _y < n and 0 <= _x < m and maps[_y][_x] == 1 and _distance < visited[_y][_x]:
                 _foot = foot.copy()
                 _foot.append((_y, _x))
-                queue.append(_foot)
+                visited[_y][_x] = _distance
+                queue.append([_foot, _distance])
 
     return answer
 
