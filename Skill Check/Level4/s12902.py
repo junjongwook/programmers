ee13f9eb3742e6802c196e04ae94ec6a4014186c
69429 <jwjun@lgcns.com>
2020-12-22 21:36:12 +0900

    문제 풀이가 쉽지가 않네. 다른 사람들 풀이를 열심히 보고 이해는 했는데 다음에 할 때도 쉽게 풀지는 못 할 거 같네

--- Skill Check/Level4/s12902.py
+++ Skill Check/Level4/s12902.py
@@ -3,16 +3,38 @@
 3 x n 타일링 : https://programmers.co.kr/learn/courses/30/lessons/12902?language=python3
 """
 
-import sys
-sys.setrecursionlimit(10 ** 6)
-def solution(n):
-    if n < 1: return 1
-    if n % 2 == 1: return 0
-    if n == 2: return 3
 
-    return solution(n-2) * 3 + solution(n-4) * 2
+def solution(n):
+    if n % 2 == 1: return 0
+
+    tiling = [0] * (n + 1)
+    tiling[0] = 1
+    tiling[1] = 0
+    tiling[2] = 3
+    tiling[3] = 0
+    tiling[4] = 11
+
+    for i in range(6, n + 1, 2):
+        tiling[i] = tiling[i-2] * 3
+        for j in range(2, i - 4 + 1, 2):
+            tiling[i] += (2 * tiling[j])
+        tiling[i] += 2
+
+        tiling[i] %= 1_000_000_007
+
+    return tiling[n]
+
 
 if __name__ == '__main__':
-    result = solution(4)
+    # result = solution(4)
+    # print(f'result = {result}')
+    # assert result == 11
+    #
+    # result = solution(6)
+    # print(f'result = {result}')
+    # assert result == 41
+
+    result = solution(8)
     print(f'result = {result}')
-    assert result == 11
+    assert result == 153
+
