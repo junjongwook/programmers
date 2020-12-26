ca53678c805fad8ff4ca2c1acf732bbd29475a82
69429 <jwjun@lgcns.com>
2020-12-26 16:11:12 +0900

    정확성의 테스트14 실패에 효율성은 테스트 5만 성공이고, 나머지는 시간초과다. 쉽지 않네.

--- Skill Check/Level4/s60060.py
+++ Skill Check/Level4/s60060.py
@@ -2,48 +2,127 @@
 """
 가사 검색 : https://programmers.co.kr/learn/courses/30/lessons/60060?language=python3
 """
-from collections import defaultdict
+
+
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.children = dict()
+
+    def hasChildCharacter(self, c):
+        if c in self.children:
+            return True
+        else:
+            return False
+
+    def getChildNode(self, c):
+        if self.hasChildCharacter(c):
+            return self.children[c]
+        else:
+            return None
+
+    def getChildren(self):
+        return list(self.children.values())
+
+    def setChildNode(self, c):
+        self.children[c] = Node(c)
+
+    def isLeaf(self):
+        if len(self.children) == 0:
+            return True
+        else:
+            return False
+
+    def __str__(self, level=0):
+        temp = '\t' * level + repr(self.value) + '\n'
+        for child in self.children.values():
+            temp += child.__str__(level + 1)
+        return temp
+
+root = Node('')     # 정방향으로 확인할 경우의 root
+tail = Node('')     # 역방향에서 확인할 경우의 root
+
+def makeTree(w):
+    curr = root
+    for c in w:
+        if not curr.hasChildCharacter(c):
+            curr.setChildNode(c)
+        curr = curr.getChildNode(c)
+
+def makeReverseTree(w):
+    curr = tail
+    index = len(w)
+    for i in range(index - 1, -1, -1):
+        if not curr.hasChildCharacter(w[i]):
+            curr.setChildNode(w[i])
+        curr = curr.getChildNode(w[i])
+
+
+def search(query):
+    if query[0] == '?':
+        return searchBackward(query)
+    else:
+        return searchForward(query)
+
+def searchForward(query):
+    answer = 0
+    width = len(query)
+    stack = [root]
+    for i in range(width - 1):
+        _stack = []
+        while stack:
+            node = stack.pop()
+            if query[i] == '?':
+                _stack.extend(node.getChildren())
+            elif node.hasChildCharacter(query[i]):
+                _stack.append(node.getChildNode(query[i]))
+        stack = _stack
+    # 마지막은 항상 '?' 이므로
+    for s in stack:
+        children = s.getChildren()
+        for child in children:
+            if child.isLeaf():
+                answer += 1
+
+    return answer
+
+
+def searchBackward(query):
+    answer = 0
+    width = len(query)
+    stack = [tail]
+    for i in range(width - 1, 0, -1):
+        _stack = []
+        while stack:
+            node = stack.pop()
+            if query[i] == '?':
+                _stack.extend(node.getChildren())
+            elif node.hasChildCharacter(query[i]):
+                _stack.append(node.getChildNode(query[i]))
+        stack = _stack
+    # 마지막은 항상 '?' 이므로
+    for s in stack:
+        children = s.getChildren()
+        for child in children:
+            if child.isLeaf():
+                answer += 1
+
+    return answer
 
 def solution(words, queries):
     answer = []
-    d = defaultdict(dict)      # startswith
-    d2 = defaultdict(dict)     # endswith
-    for word in words:
-        width = len(word)
-        for i in range(1, width):
-            _start = word[:i]
-            _last = width - i
-            _end = word[-i:]
-            _first = width - i
-            if _last in d[_start]:
-                d[_start][_last] += 1
-            else:
-                d[_start][_last] = 1
-            if _first in d2[_end]:
-                d2[_end][_first] += 1
-            else:
-                d2[_end][_first] = 1
-    # print(f'd = {d}')
-    # print(f'd2 = {d2}')
 
+    for word in words:
+        makeTree(word)
+        makeReverseTree(word)
+
+    # print(root)
+    # print(tail)
+    #
+    # print(searchForward('fro??'))
+    # print(searchBackward('????o'))
     for query in queries:
-        width = len(query)
-        if query[-1] == '?':
-            index = width - query.count('?')
-            start = query[:index]
-            last = width - index
-            if last in d[start]:
-                answer.append(d[start][last])
-            else:
-                answer.append(0)
-        elif query[0] == '?':
-            index = query.count('?')
-            end = query[index:]
-            first = index
-            if first in d2[end]:
-                answer.append(d2[end][first])
-            else:
-                answer.append(0)
+        answer.append(search(query))
 
     return answer
 
