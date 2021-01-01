fd0cf16273c8eb4692d11d99492e576eb2ecf96f
69429 <jwjun@lgcns.com>
2021-01-01 19:14:59 +0900

    방법을 여기저기 찾아서 겨우 이해하고 했는데, 다행히 시간초과는 안 나오는데 테스트 4, 24, 25, 26 이 실패네. 로직에서 문제를 찾아 봐야 겠다.

--- Skill Check/Level4/s68937.py
+++ Skill Check/Level4/s68937.py
@@ -2,48 +2,63 @@
 """
 트리 트리오 중간값 : https://programmers.co.kr/learn/courses/30/lessons/68937?language=python3
 """
-
+from collections import deque
 
 def solution(n, edges):
-    answer = 0
-    m = [[float('inf')] * (n+1) for _ in range(n+1)]
-    for h, v in edges:
-        m[h][v] = 1
-        m[v][h] = 1
-    for i in range(n+1):
-        m[0][i] = 0
-        m[i][i] = 0
-        m[i][0] = 0
+    d = dict()
+    for v1, v2 in edges:
+        d.setdefault(v1, [])
+        d.setdefault(v2, [])
+        d[v1].append(v2)
+        d[v2].append(v1)
+    # print(f'd= {d}')
 
-    for h in range(1, n):
-        for v in range(h + 1, n + 1):
-            for k in range(1, n + 1):
-                if k == h or k == v: continue
-                m[h][v] = min(m[h][v], m[h][k] + m[k][v])
-                m[v][h] = m[h][v]
+    def distance(start):
+        result = [float('inf')] * (n+1)
+        result[0] = 0
+        result[start] = 0
+        queue = deque()
+        for v in d[start]:
+            queue.append((v, 1))
+        while queue:
+            v, d2 = queue.popleft()
+            result[v] = d2
+            for _v in d[v]:
+                if result[_v] != float('inf'):
+                    continue
+                else:
+                    queue.append((_v, d2 + 1))
+        return result
 
-    # print(*m, sep='\n')
+    # 1번 에서 제일 먼 곳의 leaf node 를 찾기
+    d3 = distance(1)
+    # print(f'd3 = {d3}')
+    leaf1 = d3.index(max(d3))
+    print(f'leaf1 = {leaf1}')
 
-    _max = 0
-    for h in range(1, n):
-        for v in range(h + 1, n + 1):
-            for k in range(1, n + 1):
-                if k == h or k == v: continue
-                _sum = sum([m[h][v], m[h][k], m[k][v]])
-                if _sum > _max:
-                    _max = _sum
+    # leaf 에서 다른 leaf 을 찾기
+    d4 = distance(leaf1)
+    cnt = d4.count(max(d4))
+    # leaf 이 두개이상 나온다면 그 값이 중간값으로 반환해도 된다.
+    if cnt > 1:
+        return max(d4)
+    leaf2 = d4.index(max(d4))
 
-    answer = _max // 3
-
-    return answer
+    # 또다른 leaf 에서 leaf 이 있는지 확인
+    d5 = distance(leaf2)
+    cnt2 = d5.count(max(d5))
+    # leaf 이 두개 이상 나온다면 그 값이 중간값으로 반환해도 된다.
+    if cnt > 1:
+        return max(d5)
+    return max(d5) - 1
 
 
 if __name__ == '__main__':
-    # result = solution(4, [[1,2],[2,3],[3,4]])
-    # print(f'result = {result}')
-    # assert result == 2
-
-    result = solution(5, [[1,5],[2,5],[3,5],[4,5]])
+    result = solution(4, [[1,2],[2,3],[3,4]])
     print(f'result = {result}')
     assert result == 2
+
+    # result = solution(5, [[1,5],[2,5],[3,5],[4,5]])
+    # print(f'result = {result}')
+    # assert result == 2
 
