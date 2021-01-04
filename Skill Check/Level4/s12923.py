46d7bd243c8e3325591a7f7a4191e0f34dca708c
69429 <jwjun@lgcns.com>
2021-01-04 17:35:27 +0900

    많은 방법으로 시도 하다가 결국엔 에라토스테네스의 체 방법과 유사하게 해서 풀었는데, 정확성은 다 맞는데 효율성에서 모두 실패다. 시간초과도 아니고 실패라니..? 뭐지?

--- Skill Check/Level4/s12923.py
+++ Skill Check/Level4/s12923.py
@@ -5,21 +5,26 @@
 
 
 def solution(begin, end):
-    answer = [0] * (end - begin + 1)
-    maxNum = end // 2
+    answer = [1] * (end - begin + 1)
 
-    for n in range(maxNum, 0, -1):
-        start = begin // n * n
-        start = start + n if start < begin else start
-        start = n * 2 if n * 2 > start else start
-        for i in range(start, end + 1, n):
-            if answer[i - begin] == 0:
-                answer[i - begin] = n
+    end2 = int(end ** (1/2))
+    for i in range(2, end2 + 1):
+        start = max(i * i, (begin + i - 1) // i * i)
+        for n in range(start, end + 1, i):
+            if answer[n - begin] == 1:
+                answer[n - begin] = n // i
+
+    if begin == 1: answer[0] = 0
 
     return answer
 
 
 if __name__ == '__main__':
-    result = solution(1, 10)
-    print(f'result = {result}')
-    assert result == [0, 1, 1, 2, 1, 3, 1, 4, 3, 5]
+    # result = solution(1, 10)
+    # print(f'result = {result}')
+    # assert result == [0, 1, 1, 2, 1, 3, 1, 4, 3, 5]
+
+    result = solution(100, 120)
+    nums = range(100, 120+1)
+    for n, r in zip(nums, result):
+        print(f'{n}:{r}', sep=' ')
