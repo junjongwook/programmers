de90661e58b24c1fb3ae93129d6a0423ae925029
69429 <jwjun@lgcns.com>
2021-01-05 00:30:26 +0900

    정말 무식하게 풀기도 했지만, 케이스에 대한 생각도 놓친게 있었는데 질문목록에서 알려 줘서 겨우겨우 풀었다. 정말 무식하게 풀었는데 다른 사람 것도 한번 보자.

--- Skill Check/Level4/s42894.py
+++ Skill Check/Level4/s42894.py
@@ -5,6 +5,7 @@
 
 
 def solution(board):
+    # print(*board, sep='\n')
     answer = 0
     height = len(board)
     width = len(board[0])
@@ -21,34 +22,30 @@ def solution(board):
         시작 포인트를 기준으로 블락의 종류를 판단한다. 회전한 형태를 각각의 하나의 경우로 생각한다.
         '''
         num = board[h][w]
-        if equal(num, h, w + 1):                # 오른쪽(+1) 확인
-            if equal(num, h, w + 2):            # 오른쪽(+2) 확인
-                if equal(num, h + 1, w + 2):    # 오른쪽(+2), 아래쪽(+1) 확인
-                    return 1, False                             # 1번 type 이면서 제거 불가능
-                elif equal(num, h + 1, w):      # 오른쪽(0), 아래쪽(+1) 확인
-                    return 5, False                             # 5번 type 이면서 제거 불가능
-                elif equal(num, h + 1, 2 + 1):  # 오른쪽(+1), 아래쪽(+1) 확인
-                    return 11, False                            # 11번 type, 제거 불가능
-            elif equal(num, h + 1, w) and equal(num, h + 2, w):
-                return 2, False                                 # 2번 type, 제거 불가능
-            elif equal(num, h + 1, w + 1) and equal(num, h + 2, w + 1):
-                return 8, False                                 # 8번 type, 제거 불가능
-        elif equal(num, h + 1, w) and equal(num, h + 2, w): # 아래로 3개 연달아 있는 블록들
-            if equal(num, h + 2, w - 1):
-                return 4, True                                  # 4번 type, 제거 가능
-            elif equal(num, h + 2, w + 1):
-                return 6, True                                  # 6번 type, 제거 가능
-            elif equal(num, h + 1, w + 1):
-                return 10, False                                # 10번 type, 제거 불가능
-            elif equal(num, h + 1, w - 1):
-                return 12, False                                # 12번 type, 제거 불가능
-        elif equal(num, h + 1, w):
-            if equal(num, h +1, w + 1) and equal(num, h + 1, w + 2):
-                return 3, True                                  # 3번 type, 제거 가능
-            elif equal(num, h + 1, w - 1) and equal(num, h + 1, w - 2):
-                return 7, True                                  # 7번 type, 제거 가능
-            elif equal(num, h + 1, w - 1) and equal(num, h + 1, w + 1):
-                return 9, True                                  # 9번 type, 제거 가능
+        if equal(num, h, w + 1) and equal(num, h, w + 2) and equal(num, h + 1, w + 2):
+            return 1, False
+        elif equal(num, h, w + 1) and equal(num, h + 1, w) and equal(num, h + 2, w):
+            return 2, False
+        elif equal(num, h + 1, w) and equal(num, h + 1, w + 1) and equal(num, h + 1, w + 2):
+            return 3, True
+        elif equal(num, h + 1, w) and equal(num, h + 2, w) and equal(num, h + 2, w - 1):
+            return 4, True
+        elif equal(num, h, w + 1) and equal(num, h, w + 2) and equal(num, h + 1, w):
+            return 5, False
+        elif equal(num, h + 1, w) and equal(num, h + 2, w) and equal(num, h + 2, w + 1):
+            return 6, True
+        elif equal(num, h + 1, w) and equal(num, h + 1, w - 1) and equal(num, h + 1, w - 2):
+            return 7, True
+        elif equal(num, h, w + 1) and equal(num, h + 1, w + 1) and equal(num, h + 2, w + 1):
+            return 8, False
+        elif equal(num, h + 1, w - 1) and equal(num, h + 1, w) and equal(num, h + 1, w + 1):
+            return 9, True
+        elif equal(num, h + 1, w) and equal(num, h + 1, w + 1) and equal(num, h + 2, w):
+            return 10, False
+        elif equal(num, h, w + 1) and equal(num, h, w + 2) and equal(num, h + 1, w + 1):
+            return 11, False
+        elif equal(num, h + 1, w) and equal(num, h + 1, w - 1) and equal(num, h + 2, w):
+            return 12, False
 
         return None, False
 
@@ -171,19 +168,31 @@ def solution(board):
 
         return
 
-    for h in range(height):
-        for w in range(width):
-            if board[h][w] <= 0: continue
+    h, w = 0, 0
+    while h < height:
+        if board[h][w] > 0:
             type, erasable = kindBlock(h, w)
-            if erasable and erase(type, h, w):
+            if type != None and erasable and erase(type, h, w):
                 answer += 1
-            else:
-                visited(type, h, w)
+                w = 0
+                continue
+        w = w + 1
+        if w == width:
+            h += 1
+            w = 0
 
     return answer
 
 
 if __name__ == '__main__':
     result = solution([[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,4,0,0,0],[0,0,0,0,0,4,4,0,0,0],[0,0,0,0,3,0,4,0,0,0],[0,0,0,2,3,0,0,0,5,5],[1,2,2,2,3,3,0,0,0,5],[1,1,1,0,0,0,0,0,0,5]])
+    print(f'result = {result}')
+    assert result == 2
+
+    result = solution([[0,0,0,0,0,0,0,0,0,0],[0,0,0,2,2,0,0,0,0,0],[0,0,0,2,1,0,0,0,0,0],[0,0,0,2,1,0,0,0,0,0],[0,0,0,0,1,1,0,0,0,0],[0,0,0,0,0,0,0,0,0,0]])
+    print(f'result = {result}')
+    assert result == 1
+
+    result = solution([[0, 0, 0, 0, 0], [1, 0, 0, 2, 0], [1, 2, 2, 2, 0], [1, 1, 0, 0, 0], [0, 0, 0, 0, 0]])
     print(f'result = {result}')
     assert result == 2
