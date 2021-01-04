77a99bfbc476ab0ae180e6783c2e34fd159f1c78
69429 <jwjun@lgcns.com>
2021-01-04 18:52:04 +0900

    Prefix Sum 만 활용하면 되네, 굳이 이분법까지는 가지 않아도 된다.

--- Skill Check/Level4/s49995.py
+++ Skill Check/Level4/s49995.py
@@ -6,28 +6,22 @@
 
 def solution(cookie):
     answer = 0
-    cookieSum = [0]
+    cookieSum = [0] # prefix Sum 을 이용하기 위한 sum array
     for c in cookie:
         cookieSum.append(cookieSum[-1] + c)
 
     l, r = 1, len(cookieSum) - 1
 
-    while l < r:
-        total = cookieSum[r] - cookieSum[l-1]
-        if total % 2 == 1:      # 전체 합이 짝수가 아니면 계산할 필요 없다.
-            if cookie[l-1] > cookie[r-1]:
-                r = r - 1
-            else:
-                l = l + 1
-            continue
-        for m in range(l, r):
-            first = cookieSum[m] - cookieSum[l-1]
-            if first * 2 == total:
-                return first
-        if cookie[l-1] > cookie[r-1]:
-            r = r - 1
-        else:
-            l = l + 1
+    for l in range(1, len(cookieSum) - 1):
+        for r in range(len(cookieSum) - 1, l, -1):
+            total = cookieSum[r] - cookieSum[l-1]
+            if total <= answer * 2:             # 앞서 나온 결과보다 작은 거라면 그냥 통과다
+                break
+            if total % 2 == 1: continue         # 총합이 홀수면 2개로 똑같이 못 나눈다.
+            for m in range(l, r):
+                first = cookieSum[m] - cookieSum[l-1]
+                if first * 2 == total and answer < first:
+                    answer = first
 
     return answer
 
