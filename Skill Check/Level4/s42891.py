76161fa7a5f38642e1a03aac33e44ef8509a6684
69429 <jwjun@lgcns.com>
2021-01-10 04:32:41 +0900

    마지막 부분은 굳이 queue 로 구현하는게 아니라 인덱스 계산만 하면 되는 것이었네.

--- Skill Check/Level4/s42891.py
+++ Skill Check/Level4/s42891.py
@@ -16,7 +16,7 @@ def solution(food_times, k):
         l = len(heap)
         times, i = heapq.heappop(heap)
         if times == prev: continue
-        if (times - prev) * l < k:
+        if (times - prev) * l <= k:
             k = k - (times - prev) * l
             prev = times
         else:
@@ -27,46 +27,33 @@ def solution(food_times, k):
 
     heap = [[times - prev, i] for times, i in heap]
     heap.sort(key=lambda x: x[1])
-    # print(f'heap = {heap}')
-    q1 = deque(heap)
-    q2 = deque([])
-    for _ in range(k):
-        times, i = q1.popleft()
-        times = times - 1
-        if times > 0:
-            q2.append([times, i])
-        if len(q1) == 0:
-            if len(q2) == 0: return -1
-            q1 = q2
-            q2 = deque([])
-    else:
-        if len(q1) > 0: answer = q1[0][1]
-        elif len(q2) > 0: answer = q2[0][1]
-        else: return -1
+    # print(f'heap = {heap}, k = {k}')
+    index = k % len(heap)
+    answer = heap[index][1]
 
     return answer
 
 
 if __name__ == '__main__':
-    # result = solution([3, 1, 2], 5)
-    # print(f'result = {result}')
-    # assert result == 1
-    #
-    # result = solution([4,2,3,6,7,1,5,8], 16)
-    # print(f'result = {result}')
-    # assert result == 3
-    #
-    # result = solution([4,2,3,6,7,1,5,8], 27)
-    # print(f'result = {result}')
-    # assert result == 5
-    #
-    # result = solution([1, 1, 1], 10)
-    # print(f'result = {result}')
-    # assert result == -1
-    #
-    # result = solution([1, 1, 2], 3)
-    # print(f'result = {result}')
-    # assert result == 3
+    result = solution([3, 1, 2], 5)
+    print(f'result = {result}')
+    assert result == 1
+
+    result = solution([4,2,3,6,7,1,5,8], 16)
+    print(f'result = {result}')
+    assert result == 3
+
+    result = solution([4,2,3,6,7,1,5,8], 27)
+    print(f'result = {result}')
+    assert result == 5
+
+    result = solution([1, 1, 1], 10)
+    print(f'result = {result}')
+    assert result == -1
+
+    result = solution([1, 1, 2], 3)
+    print(f'result = {result}')
+    assert result == 3
 
     result = solution([1, 5, 5, 5, 5, 6, 7], 31)
     print(f'result = {result}')
