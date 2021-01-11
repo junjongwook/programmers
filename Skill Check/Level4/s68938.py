903ca934ce68464711eeaf9184169d4275efbe0c
69429 <jwjun@lgcns.com>
2021-01-11 21:46:36 +0900

    segment tree 로 풀어야 한다고 해서 열심히 보고 공부했는데, 이걸로 어떻게 푸는거야? 그걸 비슷하게 이용해서 풀긴 했는데 시간초과도 나오고 런타임에러도 나오네.

---
+++ Skill Check/Level4/s68938.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+문자열의 아름다움 : https://programmers.co.kr/learn/courses/30/lessons/68938?language=python3
+"""
+import sys
+sys.setrecursionlimit(10**5)
+
+d = dict()
+def solution(s):
+    global d
+    d = dict()
+    answer = 0
+    N = len(s)
+
+    def segment(_s, _e):
+        global d
+        if _s == _e: return 0   # 등록하지 않음
+        if (_s, _e) in d:
+            return d[(_s, _e)]
+
+        if s[_s] != s[_e]:
+            _count = _e - _s
+            segment(_s, _e - 1)
+            segment(_s + 1, _e)
+        else:
+            _count = max(segment(_s, _e - 1), segment(_s + 1, _e))
+        d[(_s, _e)] = _count
+
+        return _count
+
+    segment(0, N-1)
+    # print(f'd = {d}')
+    answer = sum(d.values())
+
+    return answer
+
+
+if __name__ == '__main__':
+    result = solution("baby")
+    print(f'result = {result}')
+    assert result == 9
+
+    result = solution("oo")
+    print(f'result = {result}')
+    assert result == 0
+
