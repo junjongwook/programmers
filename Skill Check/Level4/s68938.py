91736da6b9f16bced8555748a0f519aa93671a94
69429 <jwjun@lgcns.com>
2021-01-11 23:09:17 +0900

    런타임 에러는 recursive 때문에 나온거 가아서 10**6 으로 변경하니 사라졌는데 여전히 시간초과는 나오네. 점수는 47.1 로 나오네

--- Skill Check/Level4/s68938.py
+++ Skill Check/Level4/s68938.py
@@ -3,17 +3,18 @@
 문자열의 아름다움 : https://programmers.co.kr/learn/courses/30/lessons/68938?language=python3
 """
 import sys
-sys.setrecursionlimit(10**5)
+sys.setrecursionlimit(10**6)
 
 d = dict()
+answer = 0
 def solution(s):
-    global d
-    d = dict()
+    global d, answer
     answer = 0
+    d = dict()
     N = len(s)
 
     def segment(_s, _e):
-        global d
+        global d, answer
         if _s == _e: return 0   # 등록하지 않음
         if (_s, _e) in d:
             return d[(_s, _e)]
@@ -25,12 +26,13 @@ def solution(s):
         else:
             _count = max(segment(_s, _e - 1), segment(_s + 1, _e))
         d[(_s, _e)] = _count
+        answer += _count
 
         return _count
 
     segment(0, N-1)
     # print(f'd = {d}')
-    answer = sum(d.values())
+    # answer = sum(d.values())
 
     return answer
 
@@ -43,4 +45,8 @@ if __name__ == '__main__':
     result = solution("oo")
     print(f'result = {result}')
     assert result == 0
+
+    result = solution("abb")
+    print(f'result = {result}')
+    assert result == 3
 
